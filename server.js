94456710cbfe8a627d6eeb3f1aff446dad2bcab6
Your Name <your@email.com>
2026-02-25 13:26:35 +0300

    gogogo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { Pool } = require("pg");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "mypassword123";
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "karakartal94";
 
 app.use(express.json());
 
